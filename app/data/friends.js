c90e230493bffe9d26c44fcdffa7900090d6950f
Nathan Saunders <nathan_saunders@ymail.com>
2019-04-11 16:09:25 -0700

    latest changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,38 +3,35 @@
 
 var friendsData = [
   {
-    name: "Marty",
-    photo: "https://giphy.com/gifs/80s-marty-feldman-PaDc0tDWmxCW4/fullscreen",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    name: "Richard",
+    photo:
+      "https://hips.hearstapps.com/esq.h-cdn.co/assets/17/08/980x653/gallery-1487880950-missing-richard-simmons.jpg?resize=480:*",
+    scores: [5, 3, 2, 1, 2, 1, 1, 2, 5, 5]
   },
   {
     name: "Steve",
-    photo:
-      "https://giphy.com/gifs/funny-steve-cheezburger-FqscO9yPkpZRu/fullscreen",
+    photo: "https://i.gifer.com/Rjf6.gif",
     scores: [4, 3, 3, 3, 3, 2, 2, 4, 4, 4]
   },
   {
     name: "Peter",
-    photo:
-      "https://giphy.com/gifs/peter-sellers-dr-strangelove-26ybx2jZJNkZPoSsg/fullscreen",
+    photo: "https://media3.giphy.com/media/BEPaEtY8zx3dC/giphy.gif",
     scores: [5, 5, 1, 1, 1, 5, 5, 1, 1]
   },
   {
     name: "Other Peter",
     photo:
-      "https://giphy.com/gifs/maudit-maudit-peter-otoole-medak-3JsfcCZ4p6MFO/fullscreen",
+      "https://iv1.lisimg.com/image/6394335/635full-the-ruling-class-screenshot.jpg",
     scores: [1, 1, 5, 5, 5, 1, 1, 5, 5]
   },
   {
     name: "Bill",
-    photo:
-      "https://giphy.com/gifs/girls-michelle-sxsw-Eto9EuhZtddjG/fullscreen",
+    photo: "https://images-na.ssl-images-amazon.com/images/I/51eHSs7gOTL.jpg",
     scores: [5, 3, 5, 1, 5, 1, 4, 1, 3]
-  }
+  },
   {
     name: "New York",
-    photo:
-      "https://giphy.com/gifs/vh1-i-love-new-york-l3q2PJNvzvrzQP9GE/fullscreen",
+    photo: "https://i.gifer.com/DFA6.gif",
     scores: [5, 2, 1, 1, 1, 2, 5, 1, 1]
   }
 ];
