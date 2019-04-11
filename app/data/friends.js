60a1fe8673381565e5a08cd1d46d67a72811c8bd
Nathan Saunders <nathan_saunders@ymail.com>
2019-04-11 02:16:08 -0700

    Initial Commit

---
+++ app/data/friends.js
@@ -0,0 +1,42 @@
+// 5. You should save your application's data inside of `app/data/friends.js` as an array of objects.
+// Each of these objects should roughly follow the format below.
+
+var friendsData = [
+  {
+    name: "Marty",
+    photo: "https://giphy.com/gifs/80s-marty-feldman-PaDc0tDWmxCW4/fullscreen",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Steve",
+    photo:
+      "https://giphy.com/gifs/funny-steve-cheezburger-FqscO9yPkpZRu/fullscreen",
+    scores: [4, 3, 3, 3, 3, 2, 2, 4, 4, 4]
+  },
+  {
+    name: "Peter",
+    photo:
+      "https://giphy.com/gifs/peter-sellers-dr-strangelove-26ybx2jZJNkZPoSsg/fullscreen",
+    scores: [5, 5, 1, 1, 1, 5, 5, 1, 1]
+  },
+  {
+    name: "Other Peter",
+    photo:
+      "https://giphy.com/gifs/maudit-maudit-peter-otoole-medak-3JsfcCZ4p6MFO/fullscreen",
+    scores: [1, 1, 5, 5, 5, 1, 1, 5, 5]
+  },
+  {
+    name: "Bill",
+    photo:
+      "https://giphy.com/gifs/girls-michelle-sxsw-Eto9EuhZtddjG/fullscreen",
+    scores: [5, 3, 5, 1, 5, 1, 4, 1, 3]
+  }
+  {
+    name: "New York",
+    photo:
+      "https://giphy.com/gifs/vh1-i-love-new-york-l3q2PJNvzvrzQP9GE/fullscreen",
+    scores: [5, 2, 1, 1, 1, 2, 5, 1, 1]
+  }
+];
+
+module.exports = friendsData;
